c46bd2700302b2dc77ead38fbec05b316ddff45c
saeki <hikaru-1st.toshiba@docomo.ne.jp>
2024-06-05 13:51:25 +0900

    add:modelの追加

--- model/get_model.py
+++ model/get_model.py
@@ -1,6 +1,6 @@
 from experiment.utils import set_seed
 
-from .model import LightGBMClassifier, XGBoostClassifier
+from .model import LightGBMClassifier, XGBoostClassifier, CBTClassifier, SVMClassifier, AdaBoostCustomClassifier
 
 
 def get_classifier(name, *, input_dim, output_dim, model_config, seed=42, early_stopping=50, verbose=0):
@@ -9,5 +9,11 @@ def get_classifier(name, *, input_dim, output_dim, model_config, seed=42, early_
         return XGBoostClassifier(input_dim, output_dim, model_config, verbose, seed)
     elif name == "lightgbm":
         return LightGBMClassifier(input_dim, output_dim, model_config, verbose, seed)
+    elif name == "catboost":
+        return CBTClassifier(input_dim, output_dim, model_config, verbose, seed)
+    elif name == "svm":
+        return SVMClassifier(input_dim, output_dim, model_config, verbose, seed)
+    elif name == "adaboost":
+        return AdaBoostCustomClassifier(input_dim, output_dim, model_config, verbose, seed)
     else:
         raise KeyError(f"{name} is not defined.")
